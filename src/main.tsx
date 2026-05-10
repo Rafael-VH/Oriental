bc035b821ad5fa7c4e930141e650f271504c7bfe
Rafael-VH <rafaelvillahinojosa@gmail.com>
2026-05-10 19:17:00 -0400

    Add TypeScript definitions for lucide-react and initialize store in main entry point

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,9 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App";
+import { hydrateStore } from "@/services/app.service";
+
+hydrateStore();
 
 createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
